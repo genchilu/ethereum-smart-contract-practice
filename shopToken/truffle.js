118db8d688ea7f43fe4cc9dbfe194d207aae8b69
Genchi <genchilu@gmail.com>
2018-03-22 01:07:31 +0800

    Add basic frontend for shopToken

--- shopToken/truffle.js
+++ shopToken/truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
       development: {
           host: "localhost",
-          port: 9545,
+          port: 8545,
           network_id: "*", // 匹配任何network id
           from: "0x627306090abab3a6e1400e9345bc60c78a8bef57" // 指定 contract owner
        }
